c46b41228051bcb683523902bdbb4bdca6b4ebc6
Franz Koessler <fkoessler@gmail.com>
2014-06-18 16:25:01 +0200

    Add tests for Verse model.

--- spec/models/verse_spec.rb
+++ spec/models/verse_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Verse, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(create(:verse)).to be_valid
+  end
+  it "is invalid without a line_one" do
+    expect(build(:verse)).not_to be_valid
+  end
 end
