b9127e59c8d7e94c30d829a2e11267ba10d886a6
Franz Koessler <fkoessler@gmail.com>
2014-07-01 13:49:35 +0200

    added tests for Questionnaire welcome action

--- spec/controllers/questionnaire_controller_spec.rb
+++ spec/controllers/questionnaire_controller_spec.rb
@@ -2,39 +2,46 @@ require 'rails_helper'
 
 RSpec.describe QuestionnaireController, :type => :controller do
 
-  describe "GET 'receiver_name'" do
+  describe "GET 'welcome'" do
     it "returns http success" do
-      get 'receiver_name'
+      get 'welcome'
       expect(response).to be_success
     end
   end
 
-  describe "GET 'location'" do
-    it "returns http success" do
-      get 'location'
-      expect(response).to be_success
-    end
-  end
+  #describe "GET 'receiver_name'" do
+  #  it "returns http success" do
+  #    get 'receiver_name'
+  #    expect(response).to be_success
+  #  end
+  #end
 
-  describe "GET 'relationship'" do
-    it "returns http success" do
-      get 'relationship'
-      expect(response).to be_success
-    end
-  end
+  #describe "GET 'location'" do
+  #  it "returns http success" do
+  #    get 'location'
+  #    expect(response).to be_success
+  #  end
+  #end
 
-  describe "GET 'trait'" do
-    it "returns http success" do
-      get 'trait'
-      expect(response).to be_success
-    end
-  end
+  #describe "GET 'relationship'" do
+  #  it "returns http success" do
+  #    get 'relationship'
+  #    expect(response).to be_success
+  #  end
+  #end
 
-  describe "GET 'message'" do
-    it "returns http success" do
-      get 'message'
-      expect(response).to be_success
-    end
-  end
+  #describe "GET 'trait'" do
+  #  it "returns http success" do
+  #    get 'trait'
+  #    expect(response).to be_success
+  #  end
+  #end
+
+  #describe "GET 'message'" do
+  #  it "returns http success" do
+  #    get 'message'
+  #    expect(response).to be_success
+  #  end
+  #end
 
 end
--- spec/views/questionnaire/welcome.html.haml_spec.rb
+++ spec/views/questionnaire/welcome.html.haml_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "questionnaire/welcome.html.haml", :type => :view do
-  pending "it displays the form to ask for receiverName"
+
+  it "displays the receiverName input" do
+    render
+    expect(rendered).to have_field('receiver_name', :type => 'text')
+  end
+
+  it "displays a submit button" do
+    render
+    expect(rendered).to have_submit_button("Next")
+  end
+
   pending "it should contain submit button to correct url"
 end
