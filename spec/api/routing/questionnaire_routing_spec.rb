05abfe9644a88436b86ea340a92654b0c90b0c66
Franz Koessler <fkoessler@gmail.com>
2014-07-04 12:22:17 +0200

    write api questionnaire routing spec

--- spec/api/routing/questionnaire_routing_spec.rb
+++ spec/api/routing/questionnaire_routing_spec.rb
@@ -2,6 +2,14 @@ require "rails_helper"
 
 RSpec.describe Api::QuestionnaireController, :type => :routing do
   describe "routing" do
-
+    it "routes to #relationship" do
+      expect(:get => "/api/questionnaire/relationship").to route_to("api/questionnaire#relationship", :format => "json")
+    end
+    it "routes to #trait" do
+      expect(:get => "/api/questionnaire/trait").to route_to("api/questionnaire#trait", :format => "json")
+    end
+    it "routes to #message" do
+      expect(:get => "/api/questionnaire/message").to route_to("api/questionnaire#message", :format => "json")
+    end
   end
 end
